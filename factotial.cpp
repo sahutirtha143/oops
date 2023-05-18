a354d3aece8a9e6c3fa17afbf532c058a25e3067
tirtha143 <sahutirtha143@gmail.com>
2023-05-18 21:16:07 +0530

    my first attempt

---
+++ factotial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,fact=1;
+    cout<<"Enter the number to found factorial: ";
+    cin>>n;
+    if(n<0){
+        cout<<"Factorial not possible: ";
+    }
+    else{
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+    }
+    cout<<"Factorial of the number is: "<<fact;
+    cout<<" HEllo guys: ";
+    return 0;
+}
